3d53a74927c04505410c5dbb7b7f9afc2cd6aacc
LeandroChinelli19 <leandro.pereira@prevision.com.br>
2023-11-30 00:17:28 -0300

    changes in main

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ files_obj = FilePath()
 files = files_obj.get_files()
 
 df_fatura = pd.DataFrame()
+df_categories = pd.read_csv('places_categories.csv', sep=';')
 
 for file in files:
     extractor = ExtractionPDF()
@@ -17,7 +18,6 @@ df_fatura = df_fatura[~df_fatura['places'].str.contains(Constants.TOTAL_VALUE)]
 
 payment_condition = (df['places'].str.contains(f'{Constants.REGULAR_PAYMENT}|{Constants.ONLINE_PAYMENT}'))
 df.loc[payment_condition, 'values'] *= -1
-
-
+df_fatura = df_fatura.merge(df_categories, on='places', validate='m:1')
 
 df_fatura.to_csv('faturas.csv', index=False)
